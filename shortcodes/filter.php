8a383d451ac95a05a178d757e1099d8ed88db85a
root <root@Diachenko.ANIT.LOCAL>
2019-01-26 15:28:35 +1000

    Debug

--- shortcodes/filter.php
+++ shortcodes/filter.php
@@ -10,13 +10,18 @@ function zs_print_filter_n_results(){
 
     wp_add_inline_script('zoospas_handle', ZoospasFilter::zs_filert_ajax_script_string(), 'after' );
     // Подключение скриптов и стилей
+
     do_action('zoospas_enqueue');
 
+    ob_start();
     // Печать фильтра из класса
     do_action('zs_print_filter_class');
 
     // Контейнер для вывода результатов по AJAX
     do_action('zs_result_filer_box');
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
 
 }
 add_action('zs_print_filter_class', array( 'ZoospasFilter', 'zs_print_filter' ));
